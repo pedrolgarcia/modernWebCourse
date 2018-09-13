5d5ea8258a387732d139e2391c4bbd74715a487f
Pedro Lopes <pedrolopes.info@gmail.com>
2018-09-13 20:15:19 -0300

    fim bootstrap e projeto galeria

--- bootstrap/exercicios/js/navegacao.js
+++ bootstrap/exercicios/js/navegacao.js
@@ -12,6 +12,10 @@
             .then(resp => resp.text())
             .then(html => {
                 destino.innerHTML = html
+                const resultado = html.match(/\<script\>([\s\S]*)\<\/script\>/);
+                if(resultado && resultado.length >= 2) {
+                    eval(resultado[1]);
+                }
             })
     }
 
